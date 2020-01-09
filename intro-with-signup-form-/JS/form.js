dcc3e908f29c1e0cc7dc37d1e35b578eb7157945
thestriver <wannabeamumin@gmail.com>
2020-01-09 21:34:36 +0000

    intro js

---
+++ intro-with-signup-form-/JS/form.js
@@ -0,0 +1,16 @@
+function formValidation() {
+     if(document.forms["myForm"]["firstname"].value == "") {
+         alert("First Name can not be empty ");
+         document.forms["myForm"]["firstname"].focus();
+         return false;
+     };
+    
+}
+
+function validateForm() {
+    var x = document.forms["myForm"]["firstname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+}
